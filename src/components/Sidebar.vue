eae666f9fd737f8d9e83de77251755b31c602d42
Kevin O'Neil <kevinmoneil@me.com>
2018-04-10 18:58:05 +0900

    adds setUsername method back in

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -34,6 +34,9 @@ export default {
     onSubmit(event) {
       const username = event.target.username.value;
 
+      this.setUserName(username);
+    },
+    setUserName(username) {
       this.$socket.sendObj({
         command: 'NICK',
         params: username,
