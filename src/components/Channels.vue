ec7444971321cd6ed4f74ca9737e356271a42c98
Kevin O'Neil <kevinmoneil@me.com>
2018-06-19 15:49:15 +0900

    latest code

--- src/components/Channels.vue
+++ src/components/Channels.vue
@@ -1,16 +1,37 @@
 <template>
-  <nav class="Channels" />
+  <nav class="Channels">
+    <button 
+      v-if="username"
+      @click="createChannel">
+      Create Channel
+    </button>
+  </nav>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: 'Channels',
+  computed: mapState({
+    username: state => state.user.username
+  }),
   created() {
-    this.$socket.sendObj({
-      command: 'JOIN',
-      params: '#random'
-    });
-  }
+    this.$options.sockets.onmessage = (data) => {
+      this.showMessage(data);
+    }
+  },
+  methods: {
+    showMessage(data) {
+      console.log(data);
+    },
+    createChannel() {
+      this.$socket.sendObj({
+        command: 'JOIN',
+        params: '#random'
+      });
+    }
+  },
 }
 </script>
 
